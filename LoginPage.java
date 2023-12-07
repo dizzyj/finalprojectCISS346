5288fd946a65e1aea54da5dc068e1d7a6579725f
David Jenson <Davidkjensonii@gmail.com>
2023-12-07 15:43:31 -0800

    something

--- LoginPage.java
+++ LoginPage.java
@@ -1,7 +1,6 @@
 /**
  * landingPage
  */ 
-import javax.swing.Action;
 import javax.swing.JButton;  
 import javax.swing.JFrame;  
 import javax.swing.JLabel;  
--- Sub.java
+++ Sub.java
@@ -1,23 +1,45 @@
 import java.awt.GridBagConstraints;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Sub extends Page{
     int daysRemainging;
     float costPerDay;
     String username;
     JLabel daysCounter;
-
+    String [] combo = {"1 month","3 months","6 months", "1 year"};
+    JComboBox amountSelect = new JComboBox<>(combo);
+    JLabel creditCardlabel = new JLabel("Credit Card:");
+    JTextField creditField = new JTextField(16);
+    JButton purchase = new JButton("Purchase");
     Sub(JFrame f){
         super(f);
         GridBagConstraints c = new GridBagConstraints();
         title.setText("Subscription Services");
-        c.gridx =0;
         c.gridy = 0;
+        c.gridx =1;
         panel.add(title,c);
         getInfo();
         daysCounter = new JLabel("Subscription time remaining: "+String.valueOf(daysRemainging));
-        panel.add(daysCounter);
+        c.gridy = 1;
+        c.gridx = 1;
+        panel.add(daysCounter,c);
+        c.gridy = 2;
+        c.gridx = 0;
+        panel.add(amountSelect,c);
+        c.gridy = 2;
+        c.gridx = 1;
+        panel.add(creditCardlabel,c);
+        c.gridy = 2;
+        c.gridx = 2;
+        panel.add(creditField,c);
+        c.gridy = 3;
+        c.gridx = 1;
+        panel.add(purchase,c);
 
     }
     void getInfo(){
